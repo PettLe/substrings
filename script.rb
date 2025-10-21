61b7ecd2ac226b41eddd272da08c70ce9b7a5287
PettLe <90179827+PettLe@users.noreply.github.com>
2025-10-21 09:37:33 +0300

    Ready everything to start building

---
+++ script.rb
@@ -0,0 +1,7 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word, source)
+  p source
+end
+
+substrings("below", dictionary)
