67182fc86360c2ff6af0ee587c3d242169c5335e
PettLe <90179827+PettLe@users.noreply.github.com>
2025-10-21 10:07:25 +0300

    Method works with multiple words and is case insensitive

--- script.rb
+++ script.rb
@@ -1,7 +1,25 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(word, source)
-  p source
+def substrings(string, source)
+  temp_string = string.split(" ")
+  temp = Hash.new()
+
+  source.each do |item|
+    counter = 0
+    temp_string.each do |string_item|
+      if string_item.downcase.include? item
+        counter += 1
+      end
+    end
+    if counter > 0
+      temp[item] = counter
+    end
+  end
+  p temp
 end
 
-substrings("below", dictionary)
+substrings("BElow", dictionary)
+# Should be { "below" => 1, "low" => 1 }
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+# should be { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
